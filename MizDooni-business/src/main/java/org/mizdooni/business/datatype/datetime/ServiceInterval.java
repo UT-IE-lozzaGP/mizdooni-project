d346613832f0981237773787c2345027b92e94d0
A Lotov <alilotfollahi2000@gmail.com>
2024-03-25 02:10:08 +0330

    refactor: the name of some fields were altered

--- MizDooni-business/src/main/java/org/mizdooni/business/datatype/datetime/ServiceInterval.java
+++ MizDooni-business/src/main/java/org/mizdooni/business/datatype/datetime/ServiceInterval.java
@@ -4,28 +4,28 @@ package org.mizdooni.business.datatype.datetime;
 import lombok.NonNull;
 import lombok.Value;
 
+import java.time.LocalTime;
+
 @Value
 public class ServiceInterval {
-    @NonNull Integer startHour;
+    @NonNull LocalTime openingTime;
 
-    @NonNull Integer endHour;
+    @NonNull LocalTime closingTime;
 
-    public ServiceInterval(@NonNull Integer startHour, @NonNull Integer endHour) {
-        verify(startHour, endHour);
+    public ServiceInterval(@NonNull LocalTime openingTime, @NonNull LocalTime closingTime) {
+        verify(openingTime, closingTime);
 
-        this.startHour = startHour;
-        this.endHour = endHour;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
     }
 
-    private static void verify(@NonNull Integer startHour, @NonNull Integer endHour) {
-        if (!isIntervalValid(startHour, endHour)) {
+    private static void verify(@NonNull LocalTime openingTime, @NonNull LocalTime closingTime) {
+        if (!isIntervalValid(openingTime, closingTime)) {
             throw new RuntimeException("service interval is not valid");
         }
     }
 
-    private static boolean isIntervalValid(@NonNull Integer startHour, @NonNull Integer endHour) {
-        return 0 <= startHour && startHour < 24
-                && 0 < endHour && endHour <= 24
-                && startHour <= endHour;
+    private static boolean isIntervalValid(@NonNull LocalTime openingTime, @NonNull LocalTime closingTime) {
+        return openingTime.getMinute() == 0 && closingTime.getMinute() == 0 && openingTime.isBefore(closingTime);
     }
 }
