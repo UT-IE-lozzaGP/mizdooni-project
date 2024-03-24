51dc1b38cd5e1ba09817596ec94c3ead34d5e5c9
A Lotov <alilotfollahi2000@gmail.com>
2024-03-25 02:10:19 +0330

    refactor: the name of some fields were altered

--- MizDooni-business/src/main/java/org/mizdooni/business/entity/Reservation.java
+++ MizDooni-business/src/main/java/org/mizdooni/business/entity/Reservation.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
-import org.mizdooni.business.datatype.datetime.TimeSlot;
+import org.mizdooni.business.datatype.datetime.BookingTime;
 import org.mizdooni.business.datatype.id.ReservationID;
 import org.mizdooni.business.datatype.id.TableID;
 import org.mizdooni.business.datatype.id.user.CustomerID;
@@ -22,15 +22,15 @@ public class Reservation {
     private TableID tableID;
 
     @NonNull
-    private TimeSlot timeSlot;
+    private BookingTime bookingTime;
 
     @Builder
     public Reservation(@NonNull CustomerID customerID,
                        @NonNull TableID tableID,
-                       @NonNull TimeSlot timeSlot) {
+                       @NonNull BookingTime bookingTime) {
         this.customerID = customerID;
         this.tableID = tableID;
-        this.timeSlot = timeSlot;
+        this.bookingTime = bookingTime;
     }
 
 }
