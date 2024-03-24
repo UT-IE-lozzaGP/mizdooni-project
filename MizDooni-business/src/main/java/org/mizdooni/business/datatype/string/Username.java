d69f67ae77c944290a9fd174e25818fb4d1cec8d
A Lotov <alilotfollahi2000@gmail.com>
2024-03-25 01:34:14 +0330

    fix: no verification happens before instantiation

--- MizDooni-business/src/main/java/org/mizdooni/business/datatype/string/Username.java
+++ MizDooni-business/src/main/java/org/mizdooni/business/datatype/string/Username.java
@@ -2,7 +2,22 @@ package org.mizdooni.business.datatype.string;
 
 import lombok.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Value
 public class Username {
     @NonNull String value;
+
+    final private static Pattern pattern = Pattern.compile("^[a-zA-Z0-9_]{5,20}$");
+
+    public Username(@NonNull String value) {
+        verify(value);
+        this.value = value;
+    }
+    private static void verify(@NonNull String value) {
+        final Matcher matcher = pattern.matcher(value);
+        if (!matcher.matches())
+            throw new RuntimeException("username is not valid");
+    }
 }
