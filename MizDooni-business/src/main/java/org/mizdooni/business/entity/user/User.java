f1dcc9fe2325aa395dc55c1a09f73019ba45288a
A Lotov <alilotfollahi2000@gmail.com>
2024-03-24 03:51:34 +0330

    fix: abstract class User now could be built by @Builder

--- MizDooni-business/src/main/java/org/mizdooni/business/entity/user/User.java
+++ MizDooni-business/src/main/java/org/mizdooni/business/entity/user/User.java
@@ -9,9 +9,11 @@ import org.mizdooni.business.datatype.Address;
 
 @Getter
 @EqualsAndHashCode
+@Builder
 abstract class User {
     @NonNull
-    private ID id;
+    @Builder.Default
+    private final ID id = new ID();
     @NonNull @Setter
     private Username username;
     @NonNull @Setter
@@ -22,7 +24,6 @@ abstract class User {
     private Address address;
 
     protected User(@NonNull Username username, @NonNull Password password, @NonNull Email email, @NonNull Address address) {
-        this.id = new ID();
         this.username = username;
         this.password = password;
         this.email = email;
