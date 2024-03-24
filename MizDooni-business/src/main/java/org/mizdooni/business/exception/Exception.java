5f8dde519e67f3d2940ce4ba72ebfc1a15d5b3b4
A Lotov <alilotfollahi2000@gmail.com>
2024-03-24 03:50:47 +0330

    fix: Exception class modified. now it is a runtime exception + constructor without any arguments added.

--- MizDooni-business/src/main/java/org/mizdooni/business/exception/Exception.java
+++ MizDooni-business/src/main/java/org/mizdooni/business/exception/Exception.java
@@ -2,15 +2,24 @@ package org.mizdooni.business.exception;
 
 import lombok.*;
 import org.mizdooni.business.datatype.Message;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Builder
-public class Exception extends java.lang.Exception {
+public class Exception extends java.lang.RuntimeException {
     private final List<Message> messages;
 
     public Exception(@NonNull List<Message> messages) {
         this.messages = messages;
     }
 
+    public Exception() {
+        this.messages = new ArrayList<>();
+    }
+
+    public void addMessage(@NonNull Message newMessage) {
+        messages.add(newMessage);
+    }
 }
