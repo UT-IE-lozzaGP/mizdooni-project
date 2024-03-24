6036f7f18b37768328a89d3c18b0e1aae0ef03ef
A Lotov <alilotfollahi2000@gmail.com>
2024-03-25 02:13:18 +0330

    feat: email verification were added

--- MizDooni-business/src/main/java/org/mizdooni/business/datatype/string/Email.java
+++ MizDooni-business/src/main/java/org/mizdooni/business/datatype/string/Email.java
@@ -2,7 +2,22 @@ package org.mizdooni.business.datatype.string;
 
 import lombok.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Value
 public class Email {
     @NonNull String value;
+
+    final private static Pattern pattern = Pattern.compile("^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+(.[a-zA-Z0-9_]+)+$");
+
+    public Email(@NonNull String value) {
+        verify(value);
+        this.value = value;
+    }
+    private static void verify(@NonNull String value) {
+        final Matcher matcher = pattern.matcher(value);
+        if (!matcher.matches())
+            throw new RuntimeException("email is not valid");
+    }
 }
