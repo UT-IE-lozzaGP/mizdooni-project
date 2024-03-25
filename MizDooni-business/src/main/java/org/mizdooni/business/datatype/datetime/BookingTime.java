61731278d23318a7654d37762abb821a94245348
A Lotov <alilotfollahi2000@gmail.com>
2024-03-25 03:33:53 +0330

    refactor: the name of method isSlotValid changed into something more consistent

--- MizDooni-business/src/main/java/org/mizdooni/business/datatype/datetime/BookingTime.java
+++ MizDooni-business/src/main/java/org/mizdooni/business/datatype/datetime/BookingTime.java
@@ -6,19 +6,21 @@ import lombok.Value;
 
 import java.time.LocalTime;
 
-import static org.mizdooni.business.datatype.datetime.DateTimeUtils.isMinutesZero;
-
 @Value
 public class BookingTime {
-    @NonNull LocalTime startTime;
+    @NonNull LocalTime value;
 
-    public BookingTime(@NonNull LocalTime startTime) {
-        verify(startTime);
-        this.startTime = startTime;
+    public BookingTime(@NonNull LocalTime value) {
+        verify(value);
+        this.value = value;
     }
 
-    private static void verify(@NonNull LocalTime startTime) {
-        if (!isMinutesZero(startTime))
+    private static void verify(@NonNull LocalTime value) {
+        if (!isBookingValid(value))
             throw new RuntimeException("time slot is not valid");
+    }
+
+    private static boolean isBookingValid(@NonNull LocalTime value) {
+        return value.getMinute() == 0;
     }
 }
